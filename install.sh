6be793f8a564fb99e5711cc6d7407f40d866885c
Hugo Freire Gil <hugofreiregil@gmail.com>
2022-03-17 09:49:49 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd ~ \
  && curl -O -L https://github.com/TLmaK0/my_vim_config/archive/master.zip \
- && unzip -q master.zip && rm master.zip \
+ && unzip -qo master.zip && rm master.zip \
  && cp -r my_vim_config-master/.[!.]* ./ \
  && cp -r my_vim_config-master/.tmux.conf ./ \
  && rm -fr my_vim_config-master \
